e68a3768a0bc97f731bed09f18881fa54774211f
Wahab Shaikh <hey.wahabshaikh@gmail.com>
2024-08-06 23:17:52 +0530

    fix: allow checkout without sign in

--- app/api/stripe/create-checkout/route.js
+++ app/api/stripe/create-checkout/route.js
@@ -1,11 +1,33 @@
-import { NextResponse } from "next/server";
 import { createCheckout } from "@/libs/stripe";
 import { createClient } from "@/libs/supabase/server";
+import { NextResponse } from "next/server";
 
 // This function is used to create a Stripe Checkout Session (one-time payment or subscription)
 // It's called by the <ButtonCheckout /> component
 // Users must be authenticated. It will prefill the Checkout data with their email and/or credit card (if any)
 export async function POST(req) {
+  const body = await req.json();
+
+  if (!body.priceId) {
+    return NextResponse.json(
+      { error: "Price ID is required" },
+      { status: 400 }
+    );
+  } else if (!body.successUrl || !body.cancelUrl) {
+    return NextResponse.json(
+      { error: "Success and cancel URLs are required" },
+      { status: 400 }
+    );
+  } else if (!body.mode) {
+    return NextResponse.json(
+      {
+        error:
+          "Mode is required (either 'payment' for one-time payments or 'subscription' for recurring subscription)",
+      },
+      { status: 400 }
+    );
+  }
+
   try {
     const supabase = createClient();
 
@@ -13,64 +35,23 @@ export async function POST(req) {
       data: { user },
     } = await supabase.auth.getUser();
 
-    // User who are not logged in can't make a purchase
-    if (!user) {
-      return NextResponse.json(
-        { error: "You must be logged in to make a purchase." },
-        { status: 401 }
-      );
-    }
-
-    const body = await req.json();
-
     const { priceId, mode, successUrl, cancelUrl } = body;
 
-    if (!priceId) {
-      return NextResponse.json(
-        { error: "Price ID is required" },
-        { status: 400 }
-      );
-    } else if (!successUrl || !cancelUrl) {
-      return NextResponse.json(
-        { error: "Success and cancel URLs are required" },
-        { status: 400 }
-      );
-    } else if (!body.mode) {
-      return NextResponse.json(
-        {
-          error:
-            "Mode is required (either 'payment' for one-time payments or 'subscription' for recurring subscription)",
-        },
-        { status: 400 }
-      );
-    }
-
-    // Search for a profile with unique ID equals to the user session ID (in table called 'profiles')
     const { data } = await supabase
       .from("profiles")
       .select("*")
       .eq("id", user?.id)
       .single();
 
-    // If no profile found, create one. This is used to store the Stripe customer ID
-    if (!data) {
-      await supabase.from("profiles").insert([
-        {
-          id: user?.id,
-          email: user?.email,
-          price_id: body.priceId,
-        },
-      ]);
-    }
-
     const stripeSessionURL = await createCheckout({
       priceId,
       mode,
       successUrl,
       cancelUrl,
+      // If user is logged in, it will pass the user ID to the Stripe Session so it can be retrieved in the webhook later
       clientReferenceId: user?.id,
       user: {
-        email: user?.email,
+        email: data?.email,
         // If the user has already purchased, it will automatically prefill it's credit card
         customerId: data?.customer_id,
       },
--- app/api/webhook/stripe/route.js
+++ app/api/webhook/stripe/route.js
@@ -1,11 +1,13 @@
-import { NextResponse } from "next/server";
-import { headers } from "next/headers";
-import Stripe from "stripe";
-import { SupabaseClient } from "@supabase/supabase-js";
 import configFile from "@/config";
 import { findCheckoutSession } from "@/libs/stripe";
+import { SupabaseClient } from "@supabase/supabase-js";
+import { headers } from "next/headers";
+import { NextResponse } from "next/server";
+import Stripe from "stripe";
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+  apiVersion: "2023-08-16",
+});
 const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET;
 
 // This is where we receive Stripe webhook events
@@ -17,9 +19,14 @@ export async function POST(req) {
 
   const signature = headers().get("stripe-signature");
 
-  let data;
   let eventType;
   let event;
+
+  // Create a private supabase client using the secret service_role API key
+  const supabase = new SupabaseClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL,
+    process.env.SUPABASE_SERVICE_ROLE_KEY
+  );
 
   // verify Stripe event is legit
   try {
@@ -29,30 +36,55 @@ export async function POST(req) {
     return NextResponse.json({ error: err.message }, { status: 400 });
   }
 
-  data = event.data;
   eventType = event.type;
-
-  // Create a private supabase client using the secret service_role API key
-  const supabase = new SupabaseClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL,
-    process.env.SUPABASE_SERVICE_ROLE_KEY
-  );
 
   try {
     switch (eventType) {
       case "checkout.session.completed": {
         // First payment is successful and a subscription is created (if mode was set to "subscription" in ButtonCheckout)
         // ✅ Grant access to the product
-        const session = await findCheckoutSession(data.object.id);
+        const stripeObject = event.data.object;
+
+        const session = await findCheckoutSession(stripeObject.id);
 
         const customerId = session?.customer;
         const priceId = session?.line_items?.data[0]?.price.id;
-        const userId = data.object.client_reference_id;
+        const userId = stripeObject.client_reference_id;
         const plan = configFile.stripe.plans.find((p) => p.priceId === priceId);
+
+        const customer = await stripe.customers.retrieve(customerId);
 
         if (!plan) break;
 
-        // Update the profile where id equals the userId (in table called 'profiles') and update the customer_id, price_id, and has_access (provisioning)
+        let user;
+        if (!userId) {
+          // check if user already exists
+          const { data: profile } = await supabase
+            .from("profiles")
+            .select("*")
+            .eq("email", customer.email)
+            .single();
+          if (profile) {
+            user = profile;
+          } else {
+            // create a new user using supabase auth admin
+            const { data } = await supabase.auth.admin.createUser({
+              email: customer.email,
+            });
+
+            user = data?.user;
+          }
+        } else {
+          // find user by ID
+          const { data: profile } = await supabase
+            .from("profiles")
+            .select("*")
+            .eq("id", userId)
+            .single();
+
+          user = profile;
+        }
+
         await supabase
           .from("profiles")
           .update({
@@ -60,11 +92,11 @@ export async function POST(req) {
             price_id: priceId,
             has_access: true,
           })
-          .eq("id", userId);
+          .eq("id", user?.id);
 
         // Extra: send email with user link, product page, etc...
         // try {
-        //   await sendEmail({to: ...});
+        //   await sendEmail(...);
         // } catch (e) {
         //   console.error("Email issue:" + e?.message);
         // }
@@ -88,23 +120,24 @@ export async function POST(req) {
       case "customer.subscription.deleted": {
         // The customer subscription stopped
         // ❌ Revoke access to the product
+        const stripeObject = event.data.object;
         const subscription = await stripe.subscriptions.retrieve(
-          data.object.id
+          stripeObject.id
         );
 
         await supabase
           .from("profiles")
           .update({ has_access: false })
           .eq("customer_id", subscription.customer);
-
         break;
       }
 
       case "invoice.paid": {
         // Customer just paid an invoice (for instance, a recurring payment for a subscription)
         // ✅ Grant access to the product
-        const priceId = data.object.lines.data[0].price.id;
-        const customerId = data.object.customer;
+        const stripeObject = event.data.object;
+        const priceId = stripeObject.lines.data[0].price.id;
+        const customerId = stripeObject.customer;
 
         // Find profile where customer_id equals the customerId (in table called 'profiles')
         const { data: profile } = await supabase
@@ -138,7 +171,7 @@ export async function POST(req) {
       // Unhandled event type
     }
   } catch (e) {
-    console.error("stripe error: " + e.message + "EVENT TYPE: " + eventType);
+    console.error("stripe error: ", e.message);
   }
 
   return NextResponse.json({});
